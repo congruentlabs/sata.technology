f9d332ce4934b201bdebd6a60efe2ea760e84aa7
Tim <tim@congruentlabs.co>
2022-05-17 12:52:07 +1000

    update to new exchanger address

--- src/views/Vote/components/Functions/hooks.js
+++ src/views/Vote/components/Functions/hooks.js
@@ -3,7 +3,7 @@ import { Contract } from '@ethersproject/contracts';
 import EXCHANGE_ABI from './gExchangerAbi.json';
 import SATA_ABI from './sataAbi.json';
 
-const EXCHANGE_CONTRACT = '0x72dc15c9353EA3d5bc040B67d5Dd94A9E469154A';
+const EXCHANGE_CONTRACT = '0xB321feAa9018C20e94a5c4F3bf44EADEbfCF7E6C';
 const exchangeContract = new Contract(EXCHANGE_CONTRACT, EXCHANGE_ABI);
 const SATA_CONTRACT = '0x3ebb4A4e91Ad83BE51F8d596533818b246F4bEe1';
 const sataContract = new Contract(SATA_CONTRACT, SATA_ABI);
